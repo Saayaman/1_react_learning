1c46b1a86aa7531739d70e74dcaad053cd0396d1
AYAKO SAYAMA <>
2018-06-02 16:48:08 -0700

    5-1. PortFolioItem Componentを作成してImportで読み込む

---
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PortfolioItem extends Component {
+  render() {
+    return (
+      <div className="PorfolioItem">
+        Hi
+      </div>
+    )
+  }
+}
