e99b65d9e77b71a4bab6794d3bebc96f65819769
AYAKO SAYAMA <>
2018-06-03 00:16:32 -0700

    5-2. PortfolioItem の中身を作っていく+cssを追加

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import GlobalClock from './../../assets/images/global-clock.jpg';
+import './PortfolioItem.scss';
 
 export default class PortfolioItem extends Component {
   render() {
     return (
       <div className="PorfolioItem">
-        Hi
+        <div className="PortfolioItem-image">
+          <img src={GlobalClock} alt="Global Clock" />
+          <button className="PortfolioItem-button">See Project</button>
+        </div>
+        <div className="PortfolioItem-details">
+          <h3>Project Name</h3>
+          <p>Project Categories</p>
+        </div>
       </div>
     )
   }
