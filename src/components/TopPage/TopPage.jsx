5f84b7cf3787df91575517ae577a88a5bb6d2430
AYAKO SAYAMA <>
2018-06-03 12:59:42 -0700

    8. Container Componentを作ってChildrenをパスする

--- src/components/TopPage/TopPage.jsx
+++ src/components/TopPage/TopPage.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import PortfolioItem from '../PortfolioItem/PortfolioItem.jsx';
 import { portfolioItems } from '../../constants/portfolioItems.js';
+import Container from '../Container/Container.jsx';
 import './TopPage.scss';
 
 export default class TopPage extends Component {
 
   render() {
     return (
-      <div className="Container">
+      <Container>
         <div className="TopPage">
         {portfolioItems.map((portfolioItem) => (
             <PortfolioItem
@@ -18,7 +19,7 @@ export default class TopPage extends Component {
             />
           ))}
         </div>
-      </div>
+      </Container>
     )
   }
 }
