fe61b716c551f6dc994b4d6ab8ae53abab86ee83
AYAKO SAYAMA <>
2018-06-03 01:13:12 -0700

    5-3. Hover Stateをつける

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -3,12 +3,33 @@ import GlobalClock from './../../assets/images/global-clock.jpg';
 import './PortfolioItem.scss';
 
 export default class PortfolioItem extends Component {
+
+  state = {
+    isHovered: false,
+  }
+  
+  handleHoverIn = () => {
+    this.setState({
+      isHovered: true
+    });
+  }
+
+  handleHoverOut = () => {
+    this.setState({
+      isHovered: false
+    });
+  }
   render() {
+    const { isHovered  } = this.state;
     return (
-      <div className="PorfolioItem">
+      <div className="PorfolioItem" onMouseOver={this.handleHoverIn} onMouseLeave={this.handleHoverOut}>
         <div className="PortfolioItem-image">
           <img src={GlobalClock} alt="Global Clock" />
-          <button className="PortfolioItem-button">See Project</button>
+          {
+            isHovered && (
+              <button className="PortfolioItem-button">See Project</button>
+            )
+          }
         </div>
         <div className="PortfolioItem-details">
           <h3>Project Name</h3>
